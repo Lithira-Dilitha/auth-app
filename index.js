0bce1403c9f2506c921dc8013b368c433cacbadd
Lithira Dilitha <157275775+Lithira-Dilitha@users.noreply.github.com>
2024-08-19 20:17:24 +0530

    connect to database

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-app.listen(port,()=>
-    console.log(`Server is Running on http://localhost:${port}`)
+const {Pool} = require("pg");
+require('dotenv').config();
+
+const pool = new Pool({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASS,
+    port:process.env.BD_PORT
+});
+app.listen(port, () =>
+  console.log(`Server is Running on http://localhost:${port}`)
 );
