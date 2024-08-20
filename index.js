98a9dafaac135e2c3303f05b00d5d80d6d077bf5
Lithira Dilitha <157275775+Lithira-Dilitha@users.noreply.github.com>
2024-08-20 13:42:16 +0530

    implemented auth endpoint

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const {Pool} = require("pg");
+const {Issuer, generators}=require('openid-client');
 require('dotenv').config();
 
 const pool = new Pool({
@@ -11,6 +12,27 @@ const pool = new Pool({
     password:process.env.DB_PASS,
     port:process.env.BD_PORT
 });
+app.get('/auth',async (req,res)=>{
+    const googleIssuer = await Issuer.discover('https://accounts.google.com');
+
+    const client = new googleIssuer.Client({
+      client_id : process.env.CLIENT_ID,
+      client_secret : process.env.CLIENT_SECRET,
+      redirect_uris : ['http://localhost:3000/auth-callback'],
+      response_types: ['code'],
+    });
+    const codeVerifier = generators.codeVerifier();
+    const codeChalleng = generators.codeChallenge(codeVerifier);
+    const state = generators.state();
+
+    const authUrl = client.authorizationUrl({
+      scope: 'openid profile email',
+      codeChalleng,
+      code_challenge_method:'S256',
+      state,
+    });
+    res.redirect(authUrl);
+ })
 app.listen(port, () =>
   console.log(`Server is Running on http://localhost:${port}`)
 );
