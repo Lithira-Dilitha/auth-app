ace2c7bd8f635b1ec860f614de79d78290fa4d1e
Lithira Dilitha <157275775+Lithira-Dilitha@users.noreply.github.com>
2024-09-01 22:35:05 +0530

    implemented setUserContext middleware

--- src/routes/userRouters.js
+++ src/routes/userRouters.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const authenticateJwt = require('../middleware/authenticateJwt');
+const setUserContext = require('../middleware/setUserContext');
 
 const router = express.Router();
 
-router.get("/api/users",authenticateJwt,userController.getUser);
+router.get("/api/users",authenticateJwt,setUserContext,userController.getUser);
 
 module.exports = router;
