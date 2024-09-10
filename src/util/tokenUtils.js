990c2af4e8cd840436566b9d299fdf8125b8cc25
Lithira Dilitha <157275775+Lithira-Dilitha@users.noreply.github.com>
2024-09-10 20:40:20 +0530

    fixed advance token logic

--- src/util/tokenUtils.js
+++ src/util/tokenUtils.js
@@ -1,41 +1,44 @@
+const { Issuer } = require("openid-client");
 require("dotenv").config();
 
-const validateIdpToken = async (accessToken)=>{
-    try{
-        const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
-        if(!response.ok){
-            const data = await response.json();
-            return data.expiers_in > 0;
-        }
-    } catch(err){
-        console.err("Error validate IDP Token : ",err);
-        return false;
-    }
+const validateIdpToken = async (accessToken) => {
+  console.log("this is access token : " + accessToken);
+
+  try {
+    const googleIssuer = await Issuer.discover("https://accounts.google.com");
+    const client = new googleIssuer.Client({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+    });
+    const tokenSet = await client.userinfo(accessToken);
+    return tokenSet ? true : false;
+  } catch (err) {
+    console.error("Error validate IDP Token : ", err);
+    return false;
+  }
 };
 
-const resreshIdpToken = async (refreshToken)=>{
-    try{
-        const respones = await fetch('https://oauth2.googleapis.com/token',{
-            method: 'POST',
-            headers: {'Content-Type': 'pplication/x-www-form-urlencoded'},
-            body: new URLSearchParams({
-                client_id: process.env.CLIENT_ID,
-                client_secret: process.env.CLIENT_SECRET,
-                refresh_token: refreshToken,
-                grant_type: 'refresh_token',
-            }),
-        });
-        if(!respones.ok){
-            return await respones.json();
-        }
-    }catch(err){
-        console.error("Error refreshing IDP token : ",err);
-        return null;
-        
-    }
+const refreshIdpToken = async (refreshToken) => {
+  try {
+    const respones = await fetch("https://oauth2.googleapis.com/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        refresh_token: refreshToken,
+        grant_type: "refresh_token",
+      }),
+    });
+    if (!respones.ok) return null;
+    return await respones.json();
+  } catch (err) {
+    console.error("Error refreshing IDP token : ", err);
+    return null;
+  }
 };
 
 module.exports = {
-    validateIdpToken,
-    resreshIdpToken,
+  validateIdpToken,
+  refreshIdpToken,
 };
